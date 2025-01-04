3d255775d148e43543b3f1c8f06a2bc2ce2b9630
Tácio Würdig <wurdigtm@outlook.com>
2025-01-04 14:08:18 -0300

    refactor js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,76 @@
-let lanchonetes = [];
-let links = [];
-let dadosCarregados = false;
+const lanchonetes = [
+    "MackMellons Lanches",
+    "Lancheria Pelotense",
+    "Sanata Lanches",
+    "Circulus Lanches",
+    "Dada Lanches",
+    "Cannil Lanches",
+    "Petiskao Lanches",
+    "Jairo Lanches",
+    "Ronaldo Lanches",
+    "Gordo Lanches",
+    "Encontro Lanches",
+    "Porto do Lanche",
+    "PACman Lanches",
+    "Fornalha Lanches e Pizzas",
+    "Gago Lanches",
+    "Zé Rocha Lanches",
+    "Container Lanches",
+    "Tio Clóvis",
+    "Juninho's",
+    "Polonga Lanches",
+    "Wilson Lanches",
+    "Lilian Lanches",
+    "Lanches Ringo Pizzas",
+    "Bolonha Lanches",
+    "Confraria do Bauru",
+    "Catolica Lanches",
+    "Bauru na Caixinha",
+    "Sr. Bauru",
+    "Mister Bauru",
+    "Revoada lanches",
+    "Atena Laches"
+]
 
-// Função para carregar os dados
-function carregarDados() {
-    const carregarLanchonetes = fetch('./data/locais.json')
-        .then(response => response.json())
-        .then(data => {
-            lanchonetes = Object.values(data);
-        })
-        .catch(error => {
-            console.error('Erro ao carregar dados de lanchonetes:', error);
-        });
+const links = [
+    [
+        "https://www.instagram.com/mackmellonslanches",
+        "https://www.instagram.com/lancheriapelotense/?hl=pt",
+        "https://www.sanatalanches.com.br/",
+        "https://www.circuluslanches.com.br/site/",
+        "https://www.dadalanches.com.br/Site/Content/Home/",
+        "https://www.instagram.com/cannillanches1/",
+        "https://www.instagram.com/petiskao.lanches.pelotas/",
+        "https://www.instagram.com/jairolanchespelotas/",
+        "https://www.instagram.com/ronaldolanches_pel/",
+        "#",
+        "https://www.instagram.com/encontrolanches/",
+        "https://www.instagram.com/portodolanchepel/",
+        "https://www.facebook.com/profile.php?id=100064116184442",
+        "https://www.instagram.com/fornalhaoficialbr/",
+        "https://www.instagram.com/gago.lanchess/",
+        "https://www.instagram.com/zerochalanches/",
+        "https://containerlanches.com/",
+        "https://www.instagram.com/explore/locations/305640629772732/tio-clovis-lanches/?hl=pt-br",
+        "https://www.instagram.com/juninhospelotas/",
+        "https://www.instagram.com/polongalanches/",
+        "https://www.instagram.com/wilsonlanchesr/",
+        "https://lilianlanches.com.br/",
+        "https://www.instagram.com/lanchesringo/",
+        "https://www.instagram.com/bolonha_lanches/",
+        "https://www.instagram.com/confrariadobauru/",
+        "https://www.instagram.com/catolicalanches/",
+        "https://pelotas.baurunacaixinha.com.br/localidade/",
+        "https://www.instagram.com/sr.bauru/",
+        "https://www.instagram.com/mister_bauru/",
+        "https://www.instagram.com/revoada_lanchesedrinks/",
+        "https://www.instagram.com/atenalanches/"
+    ]
+]
 
-    const carregarLinks = fetch('./data/links.json')
-        .then(response => response.json())
-        .then(data => {
-            links = Object.values(data);
-        })
-        .catch(error => {
-            console.error('Erro ao carregar dados de links:', error);
-        });
-
-    // Garante que os dados sejam carregados antes de ativar o sorteio
-    Promise.all([carregarLanchonetes, carregarLinks])
-        .then(() => {
-            if (lanchonetes.length > 0 && links.length > 0) {
-                dadosCarregados = true;
-                console.log('Dados carregados com sucesso!');
-            } else {
-                console.error('Os dados não foram carregados corretamente.');
-            }
-        })
-        .catch(() => {
-            console.error('Erro ao carregar os dados.');
-        });
-}
-
-// Função para sortear
 function Sortear() {
-    if (!dadosCarregados) {
-        alert('Os dados ainda não foram carregados. Tente novamente mais tarde.');
+    if (lanchonetes.length === 0 || links.length === 0) {
+        console.error('Os dados das lanchonetes ou links não estão disponíveis.');
         return;
     }
 
@@ -56,6 +86,3 @@ function Sortear() {
     respElement.innerHTML = "";
     respElement.appendChild(linkElement);
 }
-
-// Chama a função para carregar os dados ao iniciar
-carregarDados();
