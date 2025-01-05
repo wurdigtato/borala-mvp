84e181b48d6e8f12c8d6612e8cab9e581655a82f
Tácio Würdig <wurdigtm@outlook.com>
2025-01-05 12:07:26 -0300

    Update Btns

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Sorteio Lancheria
+
 let locais = []
 let links = []
 let url
@@ -36,3 +38,54 @@ function Sortear() {
     respElement.innerHTML = `<a href="${links[numeroSorteado]}" target="_blank">${locais[numeroSorteado]}</a>`
 
 }
+
+// Modais
+
+const fadeSobre = document.getElementById('fade-sobre')
+const fadeComo = document.getElementById('fade-como')
+const modalSobre = document.getElementById('modal-sobre') 
+const btnSobre = document.getElementById('btn-sobre')
+const closeSobre = document.getElementById('close-sobre')
+const btnComoUsar = document.getElementById('btn-como-usar')
+const modalComoUsar = document.getElementById('modal-como-usar')
+const closeComoUsar = document.getElementById('close-como')
+
+function ModalSobre (){
+    fadeSobre.classList.toggle('show')
+    modalSobre.classList.toggle('show')
+}
+
+function ModalComoUsar() {
+    fadeComo.classList.toggle('show')
+    modalComoUsar.classList.toggle('show')
+}
+
+btnSobre.addEventListener('click', (e) => {
+    e.preventDefault()
+    ModalSobre()
+} )
+
+closeSobre.addEventListener('click', (e) => {
+    e.preventDefault()
+    ModalSobre()
+})
+
+fadeSobre.addEventListener('click', (e) => {
+    e.preventDefault()
+    ModalSobre()
+})
+
+btnComoUsar.addEventListener('click', (e) => {
+    e.preventDefault()
+    ModalComoUsar()
+})
+
+closeComoUsar.addEventListener('click', (e) => {
+    e.preventDefault()
+    ModalComoUsar()
+})
+
+fadeComo.addEventListener('click', (e) => {
+    e.preventDefault()
+    ModalComoUsar()
+}) 
